b076e66b8ab5a3b4f93e45e0c361143893876612
Yachiryu <daniel.castrilln@misena.edu.co>
2022-04-25 13:37:26 -0500

    Primer proyecto Unity
    Proyectp de la casita

---
+++ Casita/Assets/Scripts/MyScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    public string mensajeBienvenida;
+    public string MiNombre;
+    public string Nombrecompañero;
+    public int a;
+    public int b;
+    public bool estaArriba;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola Mundo mi nombre es:" + mensajeBienvenida + MiNombre);
+        Debug.Log("Mi edad es:" + a);
+        Debug.Log("El nombre de mi compañero es:" + Nombrecompañero);
+        Debug.Log("Y su edad es:" + b);
+        Debug.Log("Y la suma de nuestras edades es:");
+        Debug.Log(a + b);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
